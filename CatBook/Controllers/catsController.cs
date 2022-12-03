69c64bbdcaa4fca4c0b0504afba982b86a7613c5
A. Kiki <111516210+aliaa-kiki@users.noreply.github.com>
2022-12-04 00:28:34 +0300

    redid cats controller

--- CatBook/Controllers/catsController.cs
+++ CatBook/Controllers/catsController.cs
@@ -5,11 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using catbook.Models;
 using CatBook.Areas.Identity.Data;
+using catbook.Models;
 using System.Security.Claims;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 
 namespace CatBook.Controllers
 {
@@ -23,7 +21,6 @@ namespace CatBook.Controllers
         }
 
         // GET: cats
-        [Authorize]
         public async Task<IActionResult> Index()
         {
             var model = await _context.cats
@@ -33,7 +30,6 @@ namespace CatBook.Controllers
         }
 
         // GET: cats/Details/5
-        [Authorize]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null || _context.cats == null)
@@ -42,6 +38,7 @@ namespace CatBook.Controllers
             }
 
             var cat = await _context.cats
+                .Include(c => c.CatBookUser)
                 .FirstOrDefaultAsync(m => m.id == id);
             if (cat == null)
             {
@@ -52,9 +49,9 @@ namespace CatBook.Controllers
         }
 
         // GET: cats/Create
-        [Authorize]
         public IActionResult Create()
         {
+            ViewData["userId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
 
@@ -63,20 +60,19 @@ namespace CatBook.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,name,photo,about,status,vaccinated,neutered,vaccinationbook")] cat cat)
+        public async Task<IActionResult> Create([Bind("id,name,photo,about,userId,status,vaccinated,neutered,vaccinationbook")] cat cat)
         {
             if (ModelState.IsValid)
             {
-                cat.userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
                 _context.Add(cat);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["userId"] = new SelectList(_context.Users, "Id", "Id", cat.userId);
             return View(cat);
         }
 
         // GET: cats/Edit/5
-        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null || _context.cats == null)
@@ -89,6 +85,7 @@ namespace CatBook.Controllers
             {
                 return NotFound();
             }
+            ViewData["userId"] = new SelectList(_context.Users, "Id", "Id", cat.userId);
             return View(cat);
         }
 
@@ -97,8 +94,7 @@ namespace CatBook.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        [Authorize]
-        public async Task<IActionResult> Edit(int id, [Bind("id,name,photo,about,status,vaccinated,neutered,vaccinationbook")] cat cat)
+        public async Task<IActionResult> Edit(int id, [Bind("id,name,photo,about,userId,status,vaccinated,neutered,vaccinationbook")] cat cat)
         {
             if (id != cat.id)
             {
@@ -125,11 +121,11 @@ namespace CatBook.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
+            ViewData["userId"] = new SelectList(_context.Users, "Id", "Id", cat.userId);
             return View(cat);
         }
 
         // GET: cats/Delete/5
-        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null || _context.cats == null)
@@ -138,6 +134,7 @@ namespace CatBook.Controllers
             }
 
             var cat = await _context.cats
+                .Include(c => c.CatBookUser)
                 .FirstOrDefaultAsync(m => m.id == id);
             if (cat == null)
             {
@@ -150,12 +147,11 @@ namespace CatBook.Controllers
         // POST: cats/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        [Authorize]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             if (_context.cats == null)
             {
-                return Problem("Entity set 'ApplicationDbContext.cat'  is null.");
+                return Problem("Entity set 'catBookDbContext.cats'  is null.");
             }
             var cat = await _context.cats.FindAsync(id);
             if (cat != null)
